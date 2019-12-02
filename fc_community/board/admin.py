d21c9a1e84ec6911702df3c215b27b727290cdde
sseonmo <mo3509@gmail.com>
2019-12-03 00:06:43 +0900

    [fastcompus][django_basic] board-1

--- fc_community/board/admin.py
+++ fc_community/board/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'writer')
+
+
+admin.site.register(Board, BoardAdmin)
